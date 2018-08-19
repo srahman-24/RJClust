10585909f9f94553d52b09da265d051ea4f1b2eb
Shahina Rahman <38435711+anniemux@users.noreply.github.com>
2018-08-18 20:32:11 -0500

    Update main_RJ.R

--- main_RJ.R
+++ main_RJ.R
@@ -13,13 +13,7 @@ source("RJclust.R")
 Dyrskjot      = read.csv("dyrskjot-2003_database.csv", header = T, row.names = 1)
 D             = t(Dyrskjot)
 
-
-group         = c(1:nrow(D))
-group[c(1,2,5,6,8,9,11,22,34)] = 1
-group[c(3,4,7,10,12,13,14,16,17,18,19,23,26:33)] = 2
-group[c(15,20,21,24,25,35:40)] = 3
-group[c(3,13,14,30)] = 4
-group[c(4,7,12,16,18,19,23,31,33)]= 5
+# group information of the dataset Dyrskot can be obtained from README.md file. 
 
 Z  = scale(log(D), center = T, scale =T)
 
