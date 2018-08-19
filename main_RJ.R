989863c7776a2562e51adf2ea76e44e699998400
Shahina Rahman <38435711+anniemux@users.noreply.github.com>
2018-08-18 21:08:57 -0500

    Update main_RJ.R

--- main_RJ.R
+++ main_RJ.R
@@ -19,12 +19,12 @@ D             = t(Dyrskjot)
 #group1[c(1,2,5,6,8,9,11,22,34)] = 1;  #T2+
 #group1[c(3,4,7,10,12,13,14,16,17,18,19,23,26:33)] = 2; #Ta
 #group1[c(15,20,21,24,25,35:40)] = 3;  #T1
+#group = group1
 
 #group2 = group1
 #group2[c(3,13,14,30)] = 4; #Ta2
 #group2[c(4,7,12,16,18,19,23,31,33)]= 5; #Ta3
-
-group = group2;
+#group = group2;
 
 
 Z  = scale(log(D), center = T, scale =T)
