fd24661374b0d877de8212844cf4b658babf34ad
Shahina Rahman <38435711+anniemux@users.noreply.github.com>
2018-08-18 20:48:20 -0500

    Update main_RJ.R

--- main_RJ.R
+++ main_RJ.R
@@ -12,7 +12,19 @@ source("RJclust.R")
 Dyrskjot      = read.csv("Dyrskjot-2003.csv", header = T, row.names = 1)
 D             = t(Dyrskjot)
 
-# Paste the label information Source1/Source2 of the dataset Dyrskot from README.md file here. 
+# Uncomment the label information from Source1/Source2 of the dataset Dyrskjot from README.md file here. 
+
+#group1         = c(1:nrow(D));
+#group1[c(1,2,5,6,8,9,11,22,34)] = 1;  #T2+
+#group1[c(3,4,7,10,12,13,14,16,17,18,19,23,26:33)] = 2; #Ta
+#group1[c(15,20,21,24,25,35:40)] = 3;  #T1
+
+#group2 = group1
+#group2[c(3,13,14,30)] = 4; #Ta2
+#group2[c(4,7,12,16,18,19,23,31,33)]= 5; #Ta3
+
+group = group2;
+
 
 Z  = scale(log(D), center = T, scale =T)
 
