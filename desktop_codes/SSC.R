9d5f0fb62fdc7a93b0b86853de4db794f98c32fa
Shahina Rahman <38435711+srahman-24@users.noreply.github.com>
2021-04-29 19:12:51 -0500

    All the competing methods are in function now

--- desktop_codes/SSC.R
+++ desktop_codes/SSC.R
@@ -10,23 +10,14 @@
 library(RJcluster)
 
 
-km.out = kmeans(X1, centers = 4, iter.max = 1000)
-cl     = km.out$cluster
-Mutual_Information(cl, group)$ami
-
-km.out = kmeans(X, centers = 4, iter.max = 1000)
-cl     = km.out$cluster
-Mutual_Information(cl, group)$ami
-
-
 km_fx  = function(X, K){
   
-  km.out = kmeans(X, centers = K, iter.max = 1000)
-  cl     = km.out$cluster
-  return(cl)
+     km.out = kmeans(X, centers = K, iter.max = 1000)
+     cl     = km.out$cluster
+     return(cl)
 }
 
-
+Mutual_Information(cl, group)$ami
 
 
 sparcl_fx = function(X, k){
@@ -42,26 +33,17 @@ return(cl)
 Mutual_Information(cl, group)$ami
 
 
+SSC_fx = function(X, K){
 library(T4cluster)
-system.time({cl = SSC(X, k = 4)$cluster})
+    cl = SSC(X, k = K)$cluster
+    return(cl)
+}
+
 Mutual_Information(cl, group)$ami
 
 
 #system.time({cl = MSM(X, k = 4)$cluster})
 #Mutual_Information(cl, group)$ami
-
-install.packages('clustvarsel')
-library(clustvarsel)
-
-
-system.time({cl1 = clustvarsel(X1, search = 'headlong', direction = 'forward', parallel = T ,
-            emModels1 = "E", emModels2 = mclust.options("VVI"), itermax = 50, allow.EEE = F) })
-#2492.612 secs  
-
-
-system.time({cl2 = clustvarsel(X, search = 'headlong', direction = 'forward', parallel = T ,
-                               emModels1 = "E", emModels2 = mclust.options("VVI"), itermax = 50, allow.EEE = F) })
-#2492.612 secs  
 
  
 Spec_fx = function(X)
@@ -74,11 +56,11 @@ Spec_fx = function(X)
 Mutual_Information(res$assignments, group)$ami
 
 
-library(kernlab)
-spec = specc(X1, centers = 4, kernel = "vanilladot")
-
-
-
+sec_fx = function(X, K){
+         library(kernlab)
+         spec = specc(X1, centers = K, kernel = "vanilladot")
+         ## return ??
+}
 
 
 
@@ -107,6 +89,5 @@ Mutual_Information(cl$class, group)$ami
 
 
 
-## We are excited to announce our Statistics Summer Undergraduate Research Experience for BS-STAT students this summer. This provides the opportunity for undergraduate students of statistics to participate in leading-edge research.  The department is planning to fund up to 10 students this summer, each with $1500 (10 hours x 10 weeks x $15/hour) in total.
 
 
