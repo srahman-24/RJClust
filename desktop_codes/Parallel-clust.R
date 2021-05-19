e581ab058a888ab5d7500026240c5efafeac3f84
Shahina Rahman <38435711+srahman-24@users.noreply.github.com>
2021-05-19 13:57:31 -0500

    function ready to implement in the server

--- desktop_codes/Parallel-clust.R
+++ desktop_codes/Parallel-clust.R
@@ -1,3 +1,5 @@
+rm(list=ls(all=TRUE)) 
+
 library(mclust)
 library(RJcluster)
 library(clustvarsel)
@@ -11,35 +13,68 @@ numCores = detectCores()
 #register a parallel backend using one of the packages that begin with do
 registerDoParallel(numCores)
 
+
+##### function 1
+
+sim_data = function(n,p,sigma1, sigma2, seed){
+  
+  set.seed(seed)
+  group = c(rep(1, n[1]), rep(2, n[2]), rep(3, n[3]), rep(4, n[4]))
+  # Set1:  sigma = 1 ( SNR : high level) , and n  = c(20,20,20,20) 
+  # Set2:  sigma = 2 ( SNR : low level) ,  and n  = c(20,20,20,20) 
+  # Set1:  sigma = 1 ( SNR : high level) , and n  = c(20,20,200,200) (unbalanced)
+  
+  X    = matrix(rnorm(sum(n)*p,0, sigma2), nrow = sum(n), ncol = p, byrow = TRUE)
+  
+  #Cluster 1: N(2.5, sigma)(1-10), N(1.5, sigma)(11-20) 
+  
+  X[1:n[1],1:10]                                       =   rnorm(n[1]*10, 2.5, sigma1)
+  X[1:n[1],(1 + 10):(10 + 10)]                         =   rnorm(n[1]*10, 1.5, sigma1)
+  
+  #Cluster 2: N(0, sigma)(1-10), N(1.5, sigma) (11-20)
+  X[(n[1] + 1):(n[1] + n[2]),1:10]                         =   rnorm(n[2]*10, 0, sigma1)
+  X[(n[1] + 1):(n[1] + n[2]),(1 + 10):(10 + 10)]           =   rnorm(n[2]*10, 1.5, sigma1)
+  
+  #Cluster 3: N(0, sigma)(1-10), N(-1.5,sigma)(11-20)
+  X[(n[1] + n[2] + 1):(n[1] + n[2] + n[3]),1:10]               =   rnorm(n[3]*10, 0, sigma1)
+  X[(n[1] + n[2] + 1):(n[1] + n[2] + n[3]),(1 + 10):(10 + 10)] =   rnorm(n[3]*10, -1.5, sigma1)
+  
+  #Cluster 4: N(-2.5,sigma)(1-10), N(-1.5, sigma)(11-20)
+  X[(n[1] + n[2] + n[3] + 1):(n[1] + n[2] + n[3] + n[4]),1:10]                  =   rnorm(n[4]*10, -2.5, sigma1)
+  X[(n[1] + n[2] + n[3] + 1):(n[1] + n[2] + n[3] + n[4]),(1 + 10):(10 + 10)]    =   rnorm(n[4]*10, -1.5, sigma1)
+  
+  return(list(X = X, group = group))
+  
+}
+
+
+###### function 2
+
+Raftery_fx = function(X){
+  cl        =  clustvarsel(X, search = 'headlong', direction = 'forward', parallel = T ,
+                           emModels1 = "E", emModels2 = mclust.options("VVI"), itermax = 40, allow.EEE = F)
+  return(cl)
+} 
+
+
+##### main function
 set.seed(44)
 Seeds = sample(0:10000, 100, replace = FALSE)
 
-system.time({
-foreach(i = 1:10, .combine = rbind) %dopar% {
-    set.seed(Seeds[i]) 
-    rnorm(3)
-}
-})
 
-res = foreach(i = 1:5, .combine = rbind) %dopar% {
+res = foreach(i = 1:2, .combine = rbind) %dopar% {
   
-sim   = sim_data(c(20,20,20,20), 220, 1,2, Seeds[i])
-X     = sim$X 
-group = sim$group
-
-cl1    = km_fx(X, 4)
-cl2    = sparcl_fx(X, 4)
-c(Mutual_Information(cl1, group)$ami, Mutual_Information(cl2, group)$ami)
-
+  sim   = sim_data(c(20,20,20,20), 220, 1,1, Seeds[i])
+  X     = sim$X 
+  group = sim$group
+  
+  cl    = Raftery_fx(X)
+  c(Mutual_Information(cl$class, group)$ami, cl$G )
+  
 }
 
 
+save(res, file = "cvarsl_high_low.RData")
 
-#cl    = Raftery_fx(X)
-#Mutual_Information(cl$class, group)$ami
-#cl$G
 
-#cl    = HDDC_fx(X)
-#Mutual_Information(cl$class, group)$ami
-#cl$K
 
